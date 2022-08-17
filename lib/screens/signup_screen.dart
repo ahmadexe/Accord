cf76d5e9d7591046b238334b993e0e905ca2e298
Ahmad <muahmad710@gmail.com>
2022-08-17 10:31:28 +0500

    Added Signup Screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,26 +1,27 @@
-import 'package:accord/screens/signup_screen.dart';
 import 'package:accord/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          backgroundColor: Colors.white,
-          body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Center(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
                   width: double.infinity,
                   height: MediaQuery.of(context).size.height * 0.25,
                   decoration: const BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('assets/images/loginAccord.png'),
+                      image: AssetImage('assets/images/signupAccord.png'),
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -29,6 +30,34 @@ class LoginScreen extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: TextFormField(
+                    scrollPadding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom + 16*4),
+                    cursorColor: primaryColor,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      hintText: 'Enter your username',
+                      labelStyle: TextStyle(
+                        color: primaryColor,
+                        fontSize: 16,
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: primaryColor),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: primaryColor),
+                      ),
+                    ),
+                    style: const TextStyle(
+                      color: primaryColor,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    scrollPadding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom + 16*4),
                     cursorColor: primaryColor,
                     decoration: const InputDecoration(
                       labelText: 'Email',
@@ -53,6 +82,8 @@ class LoginScreen extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: TextFormField(
+                    scrollPadding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom + 16*4),
                     cursorColor: primaryColor,
                     obscureText: true,
                     decoration: const InputDecoration(
@@ -77,43 +108,33 @@ class LoginScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 30),
                 SizedBox(
-                    height: 40,
-                    width: 80,
-                    child: ElevatedButton(
-                        onPressed: () {},
-                        style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all<Color>(primaryColor)),
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(color: Colors.white),
-                        ))),
-                const SizedBox(height: 8),
+                  height: 40,
+                  width: 80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Sign Up'),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(primaryColor),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8,),
                 GestureDetector(
-                    onTap: () {Get.to(SignupScreen());},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "Don't have an account? ",
-                          style: TextStyle(
-                            color: primaryColor,
-                            fontSize: 15,
-                          ),
-                        ),
-                        Text(
-                          "Sign up!",
-                          style: TextStyle(
-                            color: secondaryColor,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    )),
+                  onTap: (){Get.back();},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('Already have an account?', style: TextStyle(color: primaryColor),),
+                      Text(' Login', style: TextStyle(color: secondaryColor, fontWeight: FontWeight.bold),)
+                    ],  
+                  ),
+                )
               ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
