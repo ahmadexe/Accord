43a0bc58ea32e9c7dbfae19fea2d40a7640af6d1
Ahmad <muahmad710@gmail.com>
2022-08-19 11:07:37 +0500

    Temporary solution -overflow

--- lib/widgets/users_rooms.dart
+++ lib/widgets/users_rooms.dart
@@ -6,31 +6,65 @@ class UsersRooms extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      width: MediaQuery.of(context).size.width * 0.90,
-      decoration: const BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.all(Radius.circular(50)),
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 0.0,
-              spreadRadius: 0.0,
-              offset: Offset(0.5, 0.5),
-            )
-          ]),
-      child: Row(
-        children: [
-          SizedBox(width: 10,),
-          CircleAvatar(
-            radius: 30,
-            backgroundColor: primaryColor,
-            child: Center(
-              child: Text('A', style: TextStyle(color: Colors.white),),
-            ),
-          )
-        ],
-      ),
-    );
+    return  Container(
+        height: 100,
+        width: MediaQuery.of(context).size.width * 0.90,
+        decoration: const BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 0.0,
+                spreadRadius: 0.0,
+                offset: Offset(0.5, 0.5),
+              )
+            ]),
+        child: Expanded(
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              const CircleAvatar(
+                radius: 30,
+                backgroundColor: primaryColor,
+                child: Center(
+                  child: Text(
+                    'A',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Expanded(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Name",
+                          style: TextStyle(
+                              color: primaryColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 60,
+                          width: MediaQuery.of(context).size.width * 0.65,
+                          child: Text('This is description ppppppppppppppppppppppppppppppppppppppppppppppp', overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                          softWrap: false,),
+                        )
+                      ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
   }
 }
